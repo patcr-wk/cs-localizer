36408420d59e8f3e6edf83cec61dd27dc200efb5
Patrick McCarthy <patrick.mccarthy@hp.com>
2022-11-04 11:02:17 -0400

    whitelist

--- public/static/js/main.60ae3ace.js
+++ public/static/js/main.60ae3ace.js
@@ -9320,7 +9320,7 @@
                       if ((console.log(e), e && e.zipfilePath)) {
                         var t = document.createElement("a");
                         (t.href =
-                          "http://localhost:5000/" +
+                          "https://cs-localizer.herokuapp.com//" +
                           e.zipfilePath),
                           document.body.appendChild(t),
                           t.click(),
